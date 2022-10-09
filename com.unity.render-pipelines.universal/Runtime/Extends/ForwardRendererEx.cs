0d9d04d5b4c2db191d6f6d9591e40be529d9c6ff
ken <redcool007@qq.com>
2022-10-09 18:57:59 +0800

    v1.0.1.2 fixed version conflict fixed posteffect not show in sceneView ui gamma flow not satisfy, will show warings 3 times.

--- com.unity.render-pipelines.universal/Runtime/Extends/ForwardRendererEx.cs
+++ com.unity.render-pipelines.universal/Runtime/Extends/ForwardRendererEx.cs
@@ -13,6 +13,7 @@ namespace UnityEngine.Rendering.Universal
         DrawObjectsPassEx drawUIObjectPass;
 
         BlitPassEx gammaPrePass, gammaPostPass;
+
         int maxWaringCount = 3;
         int waringCount = 0;
 
@@ -40,10 +41,24 @@ namespace UnityEngine.Rendering.Universal
 
         public void InitCameraGammaRendering(UniversalRendererData data)
         {
-            gammaPrePass = new BlitPassEx(nameof(gammaPrePass),RenderPassEvent.AfterRendering+10, m_BlitMaterial);
+            // remove ui layer
+            //int uiLayerId = LayerMask.GetMask("UI");
+            //if ((data.transparentLayerMask & uiLayerId) !=0)
+            //    data.transparentLayerMask &= ~uiLayerId;
 
-            drawUIObjectPass = new DrawObjectsPassEx(nameof(drawUIObjectPass), false, RenderPassEvent.AfterRendering+11, RenderQueueRange.transparent, LayerMask.GetMask("UI"), m_DefaultStencilState, data.defaultStencilState.stencilReference);
-            gammaPostPass = new BlitPassEx(nameof(gammaPostPass),RenderPassEvent.AfterRendering+20, m_BlitMaterial);
+            gammaPrePass = new BlitPassEx(nameof(gammaPrePass), RenderPassEvent.AfterRendering + 10, m_BlitMaterial);
+
+            drawUIObjectPass = new DrawObjectsPassEx(nameof(drawUIObjectPass), false, RenderPassEvent.AfterRendering + 11, RenderQueueRange.transparent, LayerMask.GetMask("UI"), m_DefaultStencilState, data.defaultStencilState.stencilReference);
+            gammaPostPass = new BlitPassEx(nameof(gammaPostPass), RenderPassEvent.AfterRendering + 20, m_BlitMaterial);
+        }
+
+        public bool IsUICamera(ref CameraData cameraData)
+        {
+            var isUICamera = QualitySettings.activeColorSpace == ColorSpace.Linear &&
+                cameraData.exData.colorSpaceUsage == ColorSpace.Gamma &&
+                cameraData.renderType == CameraRenderType.Overlay
+                ;
+            return isUICamera;
         }
 
         public void SetupCameraGammaRendering(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -52,39 +67,38 @@ namespace UnityEngine.Rendering.Universal
             if (cameraData.isSceneViewCamera)
                 return;
 
-            var isUICamera = QualitySettings.activeColorSpace == ColorSpace.Linear &&
-                cameraData.exData.colorSpaceUsage == ColorSpace.Gamma &&
-                cameraData.renderType == CameraRenderType.Overlay
-                ;
-
-            if (isUICamera )
+            if (!IsUICamera(ref cameraData))
             {
-                drawUIObjectPass.Setup(cameraData.camera.cullingMask);
-                drawUIObjectPass.RenderTarget = cameraData.exData.enableFSR ? PostProcessPass.FsrShaderConstants._EASUOutputTexture : ShaderPropertyId._FULLSIZE_GAMMA_TEX;
-
-                EnqueuePass(drawUIObjectPass);
-                
-                //remove original blit pass
-                DequeuePass(m_FinalBlitPass); // ui cammera use gammaPostPass
-
-                if (!cameraData.exData.enableFSR)
-                {
-                    gammaPrePass.SetupPrePass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
-                    EnqueuePass(gammaPrePass);
-                }
-
-                gammaPostPass.SetupPostPass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
-                EnqueuePass(gammaPostPass);
-            }
-            else
-            {
-                if(waringCount < maxWaringCount)
+                if (waringCount < maxWaringCount)
                 {
                     waringCount++;
                     Debug.LogWarning($"PowerPipeline Waring {waringCount}: Not ui camera,activeColorSpace:{QualitySettings.activeColorSpace},Camera's ColorSpaceUsage{cameraData.exData.colorSpaceUsage},CameraRenderType:{cameraData.renderType}");
                 }
+                return;
             }
-        } 
+
+            //remove original blit pass
+            DequeuePass(m_FinalBlitPass); // ui cammera use gammaPostPass
+
+            drawUIObjectPass.Setup(cameraData.camera.cullingMask);
+
+            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES3)
+                drawUIObjectPass.RenderTarget = cameraData.exData.enableFSR ? PostProcessPass.FsrShaderConstants._EASUOutputTexture : ShaderPropertyId._FULLSIZE_GAMMA_TEX;
+
+            EnqueuePass(drawUIObjectPass);
+
+            if (!cameraData.exData.enableFSR || SystemInfo.graphicsDeviceType == GraphicsDeviceType.OpenGLES3)
+            {
+                var isGenerateGammaTex = !cameraData.exData.enableFSR;
+                var renderTarget = cameraData.exData.enableFSR ? PostProcessPass.FsrShaderConstants._EASUOutputTexture : ShaderPropertyId._FULLSIZE_GAMMA_TEX;
+                gammaPrePass.SetupPrePass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment, isGenerateGammaTex, renderTarget);
+                EnqueuePass(gammaPrePass);
+            }
+
+            gammaPostPass.SetupPostPass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
+            EnqueuePass(gammaPostPass);
+
+        }
 
 
 
