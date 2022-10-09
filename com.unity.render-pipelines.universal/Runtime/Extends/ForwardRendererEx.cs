696fae76bfa8b2a5571cadb851ac5ad610d8d1d5
ken <redcool007@qq.com>
2022-10-09 11:01:46 +0800

    v1.0.1.1 fixed posteffect not show in sceneView ui gamma flow not satisfy, will show warings 3 times.

--- com.unity.render-pipelines.universal/Runtime/Extends/ForwardRendererEx.cs
+++ com.unity.render-pipelines.universal/Runtime/Extends/ForwardRendererEx.cs
@@ -13,6 +13,8 @@ namespace UnityEngine.Rendering.Universal
         DrawObjectsPassEx drawUIObjectPass;
 
         BlitPassEx gammaPrePass, gammaPostPass;
+        int maxWaringCount = 3;
+        int waringCount = 0;
 
         /// <summary>
         /// 
@@ -38,55 +40,51 @@ namespace UnityEngine.Rendering.Universal
 
         public void InitCameraGammaRendering(UniversalRendererData data)
         {
-            // remove ui layer
-            //int uiLayerId = LayerMask.GetMask("UI");
-            //if ((data.transparentLayerMask & uiLayerId) !=0)
-            //    data.transparentLayerMask &= ~uiLayerId;
-
-            gammaPrePass = new BlitPassEx(nameof(gammaPrePass), RenderPassEvent.AfterRendering+10, m_BlitMaterial);
+            gammaPrePass = new BlitPassEx(nameof(gammaPrePass),RenderPassEvent.AfterRendering+10, m_BlitMaterial);
 
             drawUIObjectPass = new DrawObjectsPassEx(nameof(drawUIObjectPass), false, RenderPassEvent.AfterRendering+11, RenderQueueRange.transparent, LayerMask.GetMask("UI"), m_DefaultStencilState, data.defaultStencilState.stencilReference);
-            gammaPostPass = new BlitPassEx(nameof(gammaPostPass), RenderPassEvent.AfterRendering+20, m_BlitMaterial);
-        }
-
-        public bool IsUICamera(ref CameraData cameraData)
-        {
-            var isUICamera = QualitySettings.activeColorSpace == ColorSpace.Linear &&
-                cameraData.exData.colorSpaceUsage == ColorSpace.Gamma &&
-                cameraData.renderType == CameraRenderType.Overlay
-                ;
-            var isSceneCamera = cameraData.isSceneViewCamera;
-            return isUICamera || isSceneCamera;
+            gammaPostPass = new BlitPassEx(nameof(gammaPostPass),RenderPassEvent.AfterRendering+20, m_BlitMaterial);
         }
 
         public void SetupCameraGammaRendering(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             ref var cameraData = ref renderingData.cameraData;
-            if (!IsUICamera(ref cameraData))
+            if (cameraData.isSceneViewCamera)
                 return;
 
-            //remove original blit pass
-            DequeuePass(m_FinalBlitPass); // ui cammera use gammaPostPass
+            var isUICamera = QualitySettings.activeColorSpace == ColorSpace.Linear &&
+                cameraData.exData.colorSpaceUsage == ColorSpace.Gamma &&
+                cameraData.renderType == CameraRenderType.Overlay
+                ;
 
-            drawUIObjectPass.Setup(cameraData.camera.cullingMask);
-
-            if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES3)
+            if (isUICamera )
+            {
+                drawUIObjectPass.Setup(cameraData.camera.cullingMask);
                 drawUIObjectPass.RenderTarget = cameraData.exData.enableFSR ? PostProcessPass.FsrShaderConstants._EASUOutputTexture : ShaderPropertyId._FULLSIZE_GAMMA_TEX;
 
-            EnqueuePass(drawUIObjectPass);
+                EnqueuePass(drawUIObjectPass);
+                
+                //remove original blit pass
+                DequeuePass(m_FinalBlitPass); // ui cammera use gammaPostPass
 
-            if (!cameraData.exData.enableFSR || SystemInfo.graphicsDeviceType == GraphicsDeviceType.OpenGLES3)
-            {
-                var isGenerateGammaTex = !cameraData.exData.enableFSR;
-                var renderTarget = cameraData.exData.enableFSR ? PostProcessPass.FsrShaderConstants._EASUOutputTexture : ShaderPropertyId._FULLSIZE_GAMMA_TEX;
-                gammaPrePass.SetupPrePass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment, isGenerateGammaTex, renderTarget);
-                EnqueuePass(gammaPrePass);
+                if (!cameraData.exData.enableFSR)
+                {
+                    gammaPrePass.SetupPrePass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
+                    EnqueuePass(gammaPrePass);
+                }
+
+                gammaPostPass.SetupPostPass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
+                EnqueuePass(gammaPostPass);
             }
-
-            gammaPostPass.SetupPostPass(cameraData.cameraTargetDescriptor, m_ActiveCameraColorAttachment);
-            EnqueuePass(gammaPostPass);
-
-        }
+            else
+            {
+                if(waringCount < maxWaringCount)
+                {
+                    waringCount++;
+                    Debug.LogWarning($"PowerPipeline Waring {waringCount}: Not ui camera,activeColorSpace:{QualitySettings.activeColorSpace},Camera's ColorSpaceUsage{cameraData.exData.colorSpaceUsage},CameraRenderType:{cameraData.renderType}");
+                }
+            }
+        } 
 
 
 
